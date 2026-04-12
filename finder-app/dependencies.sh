64e768479797989296a96de017b9f518244f9171
Oluwapelumi101 <azeezpelumi101@gmail.com>
2026-04-12 20:05:42 +0200

    Fix dependencies.sh: add aarch64 cross compiler and build tools

--- finder-app/dependencies.sh
+++ finder-app/dependencies.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Dependency installation script for kernel build.
 # Author: Siddhant Jajoo.
-
-
+sudo apt-get update
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y u-boot-tools
-sudo apt-get install -y qemu
+sudo apt-get install -y qemu-system-arm
+sudo apt-get install -y gcc-aarch64-linux-gnu
+sudo apt-get install -y binutils-aarch64-linux-gnu
+sudo apt-get install -y bc
+sudo apt-get install -y bison
+sudo apt-get install -y flex
+sudo apt-get install -y libelf-dev
+sudo apt-get install -y cpio
